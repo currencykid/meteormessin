df4cb18c19247bf2dabcb82035f7df0875dd9a79
currencykid <thecurrencykid@gmail.com>
2016-05-01 15:50:59 -0700

    Meteor Methods

--- client/main.js
+++ client/main.js
@@ -7,7 +7,7 @@ Todos = new Mongo.Collection('todos');
 
 if (Meteor.isClient){
   // template helpers
-  
+
   Template.main.helpers({
     todos: function(){
       return Todos.find({}, {sort: {createdAt: -1 }});
@@ -19,10 +19,7 @@ if (Meteor.isClient){
     "submit .new-todo": function(event){
       var text = event.target.text.value;
 
-      Todos.insert({
-        text: text,
-        createdAt: new Date()
-      });
+      Meteor.call('addTodo',text) ;
 
       //clear form
       event.target.text.value = ''; 
@@ -32,13 +29,41 @@ if (Meteor.isClient){
     },
 
     "click .toggle-checked": function(){
-      Todos.update(this._id,{$set:{checked: ! this.checked}});
+      Meteor.call('setChecked', this._id, !this.checked) ; 
     }, 
 
     "click .delete-todo": function(){
       if(confirm("Delete task?")){
-      Todos.remove(this._id); 
-    }
+        Meteor.call('deleteTodo', this._id); 
+      }
     }
   });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY" 
+  });
 }
+
+// Meteor Methods 
+Meteor.methods({
+  addTodo: function(text){
+  if(!Meteor.userId()){
+    throw new Meteor.Error('not authorized'); 
+  }
+  Todos.insert({
+    text: text,
+    createdAt: new Date(),
+    userId: Meteor.userId(),
+    username: Meteor.user().username
+  });
+},
+
+  deleteTodo:function(todoId){
+        Todos.remove(todoId); 
+  }, 
+
+  setChecked: function(todoId, setChecked){
+      Todos.update(todoId,{$set:{checked: setChecked}});
+  } 
+
+});
--- server/main.js
+++ server/main.js
@@ -4,3 +4,28 @@ Todos = new Mongo.Collection('todos');
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+// Meteor Methods 
+Meteor.methods({
+  addTodo: function(text){
+  if(!Meteor.userId()){
+    throw new Meteor.Error('not authorized'); 
+  }
+  Todos.insert({
+    text: text,
+    createdAt: new Date(),
+    userId: Meteor.userId(),
+    username: Meteor.user().username
+  });
+},
+
+  deleteTodo:function(todoId){
+        Todos.remove(todoId); 
+  }, 
+
+  setChecked: function(todoId, setChecked){
+      Todos.update(todoId,{$set:{checked: setChecked}});
+  } 
+
+});
