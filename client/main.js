16849ff8dcdf9857fdc16a17ee23017959a686bf
currencykid <thecurrencykid@gmail.com>
2016-05-01 14:52:39 -0700

    checkbox

--- client/main.js
+++ client/main.js
@@ -10,7 +10,35 @@ if (Meteor.isClient){
   
   Template.main.helpers({
     todos: function(){
-      return Todos.find();
+      return Todos.find({}, {sort: {createdAt: -1 }});
+    }
+  });
+
+
+  Template.main.events({
+    "submit .new-todo": function(event){
+      var text = event.target.text.value;
+
+      Todos.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      //clear form
+      event.target.text.value = ''; 
+
+      //Prevent submit
+      return false; 
+    },
+
+    "click .toggle-checked": function(){
+      Todos.update(this._id,{$set:{checked: ! this.checked}});
+    }, 
+
+    "click .delete-todo": function(){
+      if(confirm("Delete task?")){
+      Todos.remove(this._id); 
+    }
     }
   });
 }
